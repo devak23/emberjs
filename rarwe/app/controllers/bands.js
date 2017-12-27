6ffea079d6af64d7efe7d9c49d5583c0d68032fb
N684906 <abhay.kulkarni@jpmchhase.net>
2017-12-27 15:12:31 +0530

    added controller which does an isEmpty validations

---
+++ rarwe/app/controllers/bands.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+import { computed } from "@ember/object";
+import { isEmpty } from "@ember/utils";
+
+export default Controller.extend({
+  name: '',
+  isAddButtonDisabled: computed('name', function() {
+    return isEmpty(this.get('name'));
+  })
+});
