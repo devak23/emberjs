2abc01a1ca2ce2ec548843a3006ed178a42f4b88
devak23 <developer.ak23@gmail.com>
2017-12-28 19:05:40 +0530

    added book-details to the application

--- bookworm/app/router.js
+++ bookworm/app/router.js
@@ -7,7 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('books');
+  this.route('books', function() {
+    this.route('book', { path: ':book_id' });
+  });
 });
 
 export default Router;
--- bookworm/server/mocks/books.js
+++ bookworm/server/mocks/books.js
@@ -8,28 +8,32 @@ module.exports = function(app) {
   booksRouter.get('/', function(req, res) {
     res.send({
       'books': [{
-          "id": "978-0641723445",
+          // "id": "978-0641723445",
+          "id": "1",
           "title": "The Lightning Thief",
           "author": "Rick Riordan",
-          "description": "Percy Jackson and the Olympians",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus fuga, laudantium, odio dolor ut iusto pariatur neque similique quod ratione tempore velit iure sapiente beatae, id dolores sint quasi explicabo!",
         },
         {
-          "id": "978-1423103349",
+          // "id": "978-1423103349",
+          "id": "2",
           "title": "The Sea of Monsters",
           "author": "Rick Riordan",
-          "description": "Percy Jackson and the Olympians",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit molestiae distinctio quaerat explicabo laboriosam magni iure fuga unde qui beatae saepe, quam ipsum quos esse. Sed voluptates cumque possimus corrupti.",
         },
         {
-          "id": "978-1857995879",
+          // "id": "978-1857995879",
+          "id": "3",
           "title": "Sophie's World : The Greek Philosophers",
           "author": "Jostein Gaarder",
-          "description": "Percy Jackson and the Olympians",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, perferendis, vitae. Explicabo sit assumenda inventore, earum, alias dignissimos at, pariatur nesciunt animi veritatis molestias doloremque, aspernatur nihil cumque vero unde.",
         },
         {
-          "id": "978-1933988177",
+          // "id": "978-1933988177",
+          "id": "4",
           "title": "Lucene in Action, Second Edition",
           "author": "Michael McCandless",
-          "description": "Percy Jackson and the Olympians",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit earum ducimus aliquid laboriosam eius, rerum consequatur hic. Quae nam sint repellat, facere explicabo minima quod, aspernatur exercitationem quo iure in.",
         }
       ]
     });
