ca7e05931cfc0b3546e3ebb18578222fe5034ef2
devak23 <developer.ak23@gmail.com>
2017-12-31 22:14:07 +0530

    initial checkin after fixing the problem with the repository

---
+++ customers/app/router.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('customers', function() {
+    this.route('customer', {path: ":cust_id"});
+  });
+});
+
+export default Router;
