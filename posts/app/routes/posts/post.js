5a71ca00df990ede1c3d897eb85a05b9c2549da5
devak23 <developer.ak23@gmail.com>
2018-01-03 15:19:16 +0530

    added the next and previous buttons

--- posts/app/routes/posts/post.js
+++ posts/app/routes/posts/post.js
@@ -4,5 +4,15 @@ export default Ember.Route.extend({
   model: function(params) {
     let post = this.modelFor('posts').findBy('id', parseInt(params.post_id));
     return post;
+  },
+  actions: {
+    previousPost: function() {
+      let previous = this.controller.get('model').previous;
+      this.transitionTo(`/posts/${previous}`);
+    },
+    nextPost: function() {
+      let next = this.controller.get('model').next;
+      this.transitionTo(`/posts/${next}`);
+    }
   }
 });
