ca518cdc5f6cb2aa0879a12297b760ee18c08fc7
devak23 <developer.ak23@gmail.com>
2018-01-05 00:39:46 +0530

    initial commit with about and contact pages

---
+++ super-rentals/tests/acceptance/list-rentals-test.js
@@ -0,0 +1,34 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list-rentals');
+
+test('should link to information about the company', function(assert) {
+
+});
+
+
+test('should link to contact information', function(assert) {
+
+});
+
+test('should list available rentals', function(assert) {
+
+});
+
+test('should filter the list of rentals by city', function(assert) {
+
+});
+
+test('should show details of a selected rental', function(assert) {
+
+});
+
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
