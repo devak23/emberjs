d0005086151e09a6d71889a03d85d2c921acd6bd
devak23 <developer.ak23@gmail.com>
2018-01-07 12:30:44 +0530

    adding more tests for list-filter

--- super-rentals/tests/integration/components/list-filter-test.js
+++ super-rentals/tests/integration/components/list-filter-test.js
@@ -1,24 +1,41 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import RSVP from 'rsvp';
+import wait from "ember-test-helpers/wait";
+
+const ITEMS = [{city: 'San Francisco'}, {city: 'portland'}, {city: 'Seattle'}];
+const FILTERED_ITEMS = [{city: 'San Francisco'}];
 
 moduleForComponent('list-filter', 'Integration | Component | list filter', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('should initially load all things', function(assert) {
+  this.on('filterByCity', (val)=> {
+    if (val === '') {
+      return RSVP.resolve(ITEMS);
+    } else {
+      return RSVP.resolve({ results: ITEMS});
+    }
+  });
 
-  this.render(hbs`{{list-filter}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
   this.render(hbs`
-    {{#list-filter}}
-      template block text
+    {{#list-filter filter=(action 'filterByCity') as |results|}}
+    <ul>
+    {{#each results as |item|}}
+    <li class="city">
+    {{item.city}}
+    </li>
+    {{/each}}
+    </ul>
     {{/list-filter}}
-  `);
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+
+  return wait().then(() => {
+    assert.equal(this.$('.city').length, 3);
+    assert.equal(this.$('.city').first().text().trim(), "San Francisco");
+  });
+
 });
