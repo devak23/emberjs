723841c53a6d14b10c2812cc155b13292641caa1
devak23 <developer.ak23@gmail.com>
2018-01-08 09:32:04 +0530

    added an index route to /rentals

--- super-rentals/app/routes/rentals.js
+++ super-rentals/app/routes/rentals.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    // this will retrieve data from 'api/rentals'
-    // which has been defined in the mirage configuration
-    // 'config.js'.
-    return this.store.findAll('rental');
-
-    // The store service is injected into all routes and
-    // components in emberjs
-  }
 });
